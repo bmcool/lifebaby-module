659daba70ad017743541bc31cc3e9d3f3eb125c0
Zephyr Lin <zephyrlin@coevo.com.tw>
2013-03-23 17:35:25 +0800

    Initial Commit

---
+++ module/module.h
@@ -0,0 +1,13 @@
+//
+//  module.h
+//  module
+//
+//  Created by Lin Chi-Cheng on 13/3/23.
+//  Copyright (c) 2013年 lifebaby. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface module : NSObject
+
+@end
---
+++ module/module.m
@@ -0,0 +1,13 @@
+//
+//  module.m
+//  module
+//
+//  Created by Lin Chi-Cheng on 13/3/23.
+//  Copyright (c) 2013年 lifebaby. All rights reserved.
+//
+
+#import "module.h"
+
+@implementation module
+
+@end
